32b322e600dbbc964c56ee23cd0b1f6b4016b0aa
ddc0010 <ddc0010@alu.medac.es>
2024-03-19 16:32:30 +0100

    Main implementado 2

--- Figuras/src/app/Main.java
+++ Figuras/src/app/Main.java
@@ -1,10 +1,19 @@
 package app;
 
+import modelo.Rectangulo;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Rectangulo r1 = new Rectangulo();
+		Rectangulo r2 = new Rectangulo(2,3);
+		
+		int area1 = r1.getArea();
+		int perimetro2 = r2.getPerimetro();
+		
+		System.out.println("El area del rectangulo 1 es" + area1);
+		System.out.println("El perimetro del rectangulo 2 es" + perimetro2);
 	}
 
 }
