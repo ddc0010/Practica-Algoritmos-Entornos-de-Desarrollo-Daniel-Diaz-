1be0b7947ac0ee7f8cb60e1d49aa4fadd1de21ce
ddc0010 <ddc0010@alu.medac.es>
2024-03-19 17:21:27 +0100

    Main implementado de Triangulo

--- Figuras/src/app/CirculoMain.java
+++ Figuras/src/app/CirculoMain.java
@@ -7,7 +7,7 @@ public class CirculoMain {
 	public static void main(String[] args) {
 		
 		Circulo c1 = new Circulo();
-		Circulo r2 = new Circulo();
+		Circulo c2 = new Circulo();
 		
 		int area1 = c1.getArea();
 		int perimetro2 = c2.getPerimetro();
--- Figuras/src/app/Main.java
+++ Figuras/src/app/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Rectangulo r1 = new Rectangulo();
-		Rectangulo r2 = new Rectangulo(2,3);
+		Rectangulo r2 = new Rectangulo();
 		
 		int area1 = r1.getArea();
 		int perimetro2 = r2.getPerimetro();
