ab3a803232f8c673b24f3a2cd54518e59f5f0ec4
ddc0010 <ddc0010@alu.medac.es>
2024-04-09 17:26:57 +0200

    proyectos algoritmos creado

---
+++ Algoritmos/src/util/Algoritmos.java
@@ -0,0 +1,17 @@
+package util;
+
+public abstract class Algoritmos {
+	
+	/**
+	 * Devuelve el sumatorio del numero especificado 
+	 * @param numero el numero del que queremos calcular el sumatorio 
+	 * @return el sumatorio del numero especificado 
+	 */
+	
+	public static int sumatorio(int numero) {
+		
+		return 0;
+		
+	}
+
+}
