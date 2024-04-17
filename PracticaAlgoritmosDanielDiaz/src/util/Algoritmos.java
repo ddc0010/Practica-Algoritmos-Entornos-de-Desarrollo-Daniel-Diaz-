0a2557d22d11e419649844b939b4631b9c415203
ddc0010 <ddc0010@alu.medac.es>
2024-04-17 19:43:34 +0200

    Metodo Fibonacci

--- PracticaAlgoritmosDanielDiaz/src/util/Algoritmos.java
+++ PracticaAlgoritmosDanielDiaz/src/util/Algoritmos.java
@@ -10,5 +10,12 @@ public abstract class Algoritmos {
         else
             return num * factorial(num - 1);
     }
+	
+	 public static int fibonacci(int numero) {
+	        if (numero <= 1)
+	            return numero;
+	        else
+	            return fibonacci(numero - 1) + fibonacci(numero - 2);
+	    }
 
 }
