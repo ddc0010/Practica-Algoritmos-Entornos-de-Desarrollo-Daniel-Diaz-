146f9e1c6cea9f78a0bd5e3754551f0d7e88a680
ddc0010 <ddc0010@alu.medac.es>
2024-04-17 19:38:29 +0200

    Metodo Factorial

--- PracticaAlgoritmosDanielDiaz/src/util/Algoritmos.java
+++ PracticaAlgoritmosDanielDiaz/src/util/Algoritmos.java
@@ -1,5 +1,14 @@
 package util;
 
 public abstract class Algoritmos {
+	
+	//METODO DE FACTORIAL 
+	
+	public static int factorial(int num) {
+        if (num == 0)
+            return 1;
+        else
+            return num * factorial(num - 1);
+    }
 
 }
