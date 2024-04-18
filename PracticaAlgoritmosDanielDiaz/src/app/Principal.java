9224dd1df95c731cbbce0e5952ad84518b1e6f4d
ddc0010 <ddc0010@alu.medac.es>
2024-04-18 15:32:25 +0200

    Main creado

--- PracticaAlgoritmosDanielDiaz/src/app/Principal.java
+++ PracticaAlgoritmosDanielDiaz/src/app/Principal.java
@@ -2,6 +2,8 @@ package app;
 
 import java.util.Random;
 
+import util.Algoritmos;
+
 public class Principal {
 
 	public static void main(String[] args) {
@@ -10,8 +12,15 @@ public class Principal {
 	        Random random = new Random();
 	        for (int i = 0; i < num.length; i++) {
 	            num[i] = random.nextInt(10) + 1;
+	            
+	            int [] FibonacciResultado = new int[5];
+	            int [] FactorialResultado = new int[5];
+	            boolean[] PrimosResultado = new boolean[5];
+
+	            FibonacciResultado[i] = Algoritmos.fibonacci(num[i]);
+	            FactorialResultado[i] = Algoritmos.factorial(num[i]);
+	            PrimosResultado[i] = Algoritmos.esPrimo(num[i]);
 
 	}
 	}
-}
-
+	}
