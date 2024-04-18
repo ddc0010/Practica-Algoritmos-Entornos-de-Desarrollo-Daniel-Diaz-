301766a8eff766df2d6b3e6a861c70185dbf3693
ddc0010 <ddc0010@alu.medac.es>
2024-04-18 16:19:53 +0200

    Metodos de los tests

--- PracticaAlgoritmosDanielDiaz/src/app/Principal.java
+++ PracticaAlgoritmosDanielDiaz/src/app/Principal.java
@@ -8,18 +8,20 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		 int[] num = new int[5];
+		 //Primero creo el array con 5 numeros aleatorios del 1 al 10 
+		int[] numeros = new int[5];
 	        Random random = new Random();
-	        for (int i = 0; i < num.length; i++) {
-	            num[i] = random.nextInt(10) + 1;
+	        for (int i = 0; i < numeros.length; i++) {
+	            numeros[i] = random.nextInt(10) + 1;
 	            
-	            int [] FibonacciResultado = new int[5];
-	            int [] FactorialResultado = new int[5];
-	            boolean[] PrimosResultado = new boolean[5];
-
-	            FibonacciResultado[i] = Algoritmos.fibonacci(num[i]);
-	            FactorialResultado[i] = Algoritmos.factorial(num[i]);
-	            PrimosResultado[i] = Algoritmos.esPrimo(num[i]);
+	            //Calcula mediante un bucle el número de fibonacci de los 5 números del array  y almacena el resultado en otro array de enteros llamado resultadoFibonacci, resultadoFactorial y resultadoPrimos
+	            int [] resultadoFibonacci = new int[5];
+	            int [] resultadoFactorial = new int[5];
+	            boolean[] resultadoPrimos = new boolean[5];
+	            
+	            resultadoFibonacci[i] = Algoritmos.fibonacci(numeros[i]);
+	            resultadoFactorial[i] = Algoritmos.factorial(numeros[i]);
+	            resultadoPrimos[i] = Algoritmos.esPrimo(numeros[i]);
 
 	}
 	}
---
+++ PracticaAlgoritmosDanielDiaz/src/tests/testsAlgoritmos.java
@@ -0,0 +1,37 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Test;
+
+import util.Algoritmos;
+
+class testsAlgoritmos {
+	
+	@Test
+	void testFibonacci() {
+       int fibonacciEspera = 3;
+       int fibonacciObtiene = Algoritmos.fibonacci(5);
+       assertEquals(fibonacciEspera, fibonacciObtiene);
+    }
+    
+	@Test
+	void testFactorial() {
+	       int factorialEspera = 4;
+	       int factorialObtiene = Algoritmos.factorial(29);
+	       assertEquals(factorialEspera, factorialObtiene);
+    }
+	
+	@Test
+	void testesPrimo() {
+	       int numero = 7;
+	       boolean primoEspera = true;
+	       boolean PrimoObtiene = Algoritmos.esPrimo(numero);
+	       assertEquals(primoEspera, PrimoObtiene);
+    }
+    
+
+	}
+		
+	}
+
